0149ae02cc77aff1d5903ffc67fac33eacc2c3e7
osayami <24865842+osayami@users.noreply.github.com>
2025-12-18 01:24:19 +0530

    remove git commit from deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,3 @@ mkdir docs
 cp -r bin/Release/net10.0/publish/wwwroot/* docs/
 touch docs/.nojekyll
 git add docs
-git commit -m "Update GitHub Pages build"
-git push
-echo "Deployment to GitHub Pages completed."
