32be5aef6c18393ab4ca80fd81dbc3f1d0eaee67
osayami <24865842+osayami@users.noreply.github.com>
2025-12-18 01:34:38 +0530

    added back git commit message commands to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-dotnet publish -c Release
+dotnet publish -c Release OsayamiBlog.csproj
 rm -rf docs
 mkdir docs
 cp -r bin/Release/net10.0/publish/wwwroot/* docs/
 touch docs/.nojekyll
 git add docs
+git commit -m "Update GitHub Pages build"
+git push
+echo "Deployment to GitHub Pages completed."
