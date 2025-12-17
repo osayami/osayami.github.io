1a87443fe5e399d6e3bf4a138c2df51a1e0d904b
osayami <24865842+osayami@users.noreply.github.com>
2025-12-18 01:17:45 +0530

    added deploay scipt and set up proper routes

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dotnet publish -c Release
+rm -rf docs
+mkdir docs
+cp -r bin/Release/net10.0/publish/wwwroot/* docs/
+touch docs/.nojekyll
+git add docs
+git commit -m "Update GitHub Pages build"
+git push
+echo "Deployment to GitHub Pages completed."
